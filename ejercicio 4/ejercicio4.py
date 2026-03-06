d782f212ac6414a6f62211aa90a7fac9f2f9a902
heidyacosta <heidyacosta@colegioguaenta.edu.co>
2026-03-06 09:47:23 -0500

    se actualizo el README con analisis y diseño

---
+++ ejercicio 4/ejercicio4.py
@@ -0,0 +1,12 @@
+h =float(input("inicie la altura inicial de la pelota"))
+
+altura = h
+limite = h / 5
+rebote = 0
+
+while altura >= limite:
+    altura = altura * 0.9
+    rebote += 1
+
+("la pelota deja de alcanzar la quinta parte de la altura inicial en rebote:", rebote)
+
